5c14f80e328e0c471ca666e9f8380d4342600e8f
Waqas-khan786 <waqas.khannop@gmail.com>
2023-03-03 23:10:55 +0500

    successfully removed Functions and commented text in app.py python file

--- ai/app.py
+++ ai/app.py
@@ -1,10 +1,8 @@
 from flask import Flask,render_template,redirect,request,url_for,jsonify
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
-import datetime
+import pyttsx3
+import speech_recognition as sr
 import webbrowser
-import os
-import smtplib
+
 
 
 app = Flask(__name__)
@@ -17,26 +15,7 @@ def home():
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
-
-
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
-        speak("Good Morning!")
-
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")   
-
-    else:
-        speak("Good Evening!")  
-
-    speak("I am Jarvis Sir. Please tell me how may I help you")       
+engine.setProperty('voice', voices[0].id)  
 
 @app.route('/takeCommand',methods = ['POST'])
 def takeCommand():
@@ -89,7 +68,6 @@ def takeCommand():
     except Exception as e:
         print(e)    
         print("Say that again please...")
-        speak('Say that again please...')
         #return "None"
     return jsonify("Working")  
 if __name__ == "__main__":
